617baf16f349b2c54a462e9a322fd5802ca06444
Cole McKinnon <Home@Coles-iMac.local>
2020-11-16 20:09:37 -0800

    Added sample Login route. Toolbar needs updating

--- react/src/App.js
+++ react/src/App.js
@@ -5,17 +5,32 @@ import NewsBar from "./Components/NewsBar/NewsBar.js";
 import GainersBar from "./Components/GainersBar/GainersBar.js";
 import LosersBar from "./Components/LosersBar/LosersBar.js";
 import GraphBar from "./Components/GraphBar/GraphBar.js";
+import Login from "./Components/Login/Login.js";
+import Login2 from "./Components/Login/Login2.js";
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
     return (
-        <div className = "app-content">
-            <NavBar/>
-            <NewsBar />
-            <GainersBar/>
-            <LosersBar />
-            <GraphBar />
-        </div>
+        <Router>
+            <div className = "app-content">
+                <Switch>
+
+
+
+                    
+                
+                    <Route path="/login" exact component={Login2}/>
+                   
+ 
+                    <NavBar/>
+                    <NewsBar />
+                    <GainersBar/>
+                    <LosersBar />
+                    <GraphBar />
+                </Switch>
+            </div>
+        </Router>
   );
 }
 
