3771b27e02038e176a3a1abba4fad787de7887b7
USCS-529 <USCS-529@scs.com>
2021-03-18 16:00:53 -0700

    losers & gainers component

--- react/src/Components/Losers/Losers.js
+++ react/src/Components/Losers/Losers.js
@@ -1,20 +1,80 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import styled from 'styled-components';
+import axios from 'axios';
+import Table from './Table.js';
 import './Losers.css';
+
+const Styles = styled.div`
+  table {
+    width: 100%;
+    border-spacing: 0;
+    border: 1px solid black;
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+    th,
+    td {
+      margin: 0;
+      padding: 1rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+
 
 
 function Losers() {
+
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'SYMBOL',
+                accessor: 'symbol'
+            }
+            , {
+                Header: 'PRICE',
+                accessor: 'iexRealtimePrice'
+            }
+            , {
+                Header: 'CHANGE',
+                accessor: 'change'
+            }
+            , {
+                Header: 'PERCENT',
+                accessor: 'changePercent'
+            }
+        ]
+    )
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await axios("https://cloud.iexapis.com/stable/stock/market/list/losers?token=pk_2d87808402a3463ab504dac6eb52b540");
+            setData(result.data);
+        })();
+    }, []);
+
     return (
         <div className="losers-container">
             <h3>Today's Biggest Losers</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Pulvinar elementum integer enim neque.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Pulvinar elementum integer enim neque.</p>
+            <Styles>
+                <Table
+                    data={data}
+                    columns={columns}
+                />
+            </Styles>
         </div>
     )
 }
-
 
 export default Losers;
