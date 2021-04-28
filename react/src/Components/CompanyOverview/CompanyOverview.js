66a1cac62c052ab3cf2565e20e2c62116b35111c
Josue Chavarria <JosueChavarria@Outlook.com>
2021-04-27 21:03:44 -0700

    new stuff

---
+++ react/src/Components/CompanyOverview/CompanyOverview.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './CompanyOverview.css';
+
+
+class CompanyOverview extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tickerSymbol: 'TSLA',
+            logo: [{}],
+            isloaded: false
+        }
+    }
+
+    componentDidMount() {
+        var sandboxMode = false
+        var baseURL
+        var token
+        const logo = `/stock/${this.state.tickerSymbol}/logo?&token=`
+        const company = `/stock/${this.state.tickerSymbol}/company?&token=`
+
+        if (sandboxMode) {
+            baseURL = 'https://sandbox.iexapis.com/v1'
+            token = 'Tpk_a909e54fc2ab44ac976155957da2a605'
+        }
+        else {
+            baseURL = 'https://cloud.iexapis.com/v1'
+            token = 'pk_2d87808402a3463ab504dac6eb52b540'
+        }
+
+        const logoURL = baseURL + logo + token
+        const companyInfoURL = baseURL + company + token
+
+        fetch(logoURL)
+            .then(response => response.json())
+            .then(data => this.setState({ logo: data }))
+        fetch(companyInfoURL)
+            .then(response => response.json())
+            .then(data => this.setState({ companyInfo: data, isLoaded: true }))
+    }
+
+    render() {
+        return (
+            this.state.isLoaded ?
+                <div className="company-overview">
+                    <div className="">
+                        <img className="company-logo" src={this.state.logo.url}></img>
+                        <p>{this.state.companyInfo.description}</p>
+                    </div>
+                </div>
+                : <div></div>
+        );
+    }
+}
+
+
+export default CompanyOverview;
--- react/src/WebPages/ResearchPage/ResearchPageCompanyOverview.js
+++ react/src/WebPages/ResearchPage/ResearchPageCompanyOverview.js
@@ -4,15 +4,54 @@ import NavBar from "../../Components/NavBar/NavBar.js";
 import ResearchHeader from "../../Components/ResearchHeader/ResearchHeader.js";
 
 
-function ResearchPageCompanyOverview() {
-    return (
-        <div class="research-page-company-overview">
-            <NavBar />
-            <div className="research-page-company-overview-column-container">
-                <ResearchHeader />
-            </div>
-        </div>
-    );
+class ResearchPageCompanyOverview extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tickerSymbol: 'TSLA',
+            logo: [{}],
+            isloaded: false
+        }
+    }
+
+    componentDidMount() {
+        var sandboxMode = true
+        var baseURL
+        var token
+        const keyStats = `/stock/${this.state.tickerSymbol}/stats/companyname?&token=`
+
+        if (sandboxMode) {
+            baseURL = 'https://sandbox.iexapis.com/v1'
+            token = 'Tpk_a909e54fc2ab44ac976155957da2a605'
+        }
+        else {
+            baseURL = 'https://cloud.iexapis.com/v1'
+            token = 'pk_2d87808402a3463ab504dac6eb52b540'
+        }
+
+        const keyStatsURL = baseURL + keyStats + token
+
+        fetch(keyStatsURL)
+            .then(response => response.json())
+            .then(data => this.setState({ keyStats: data, isLoaded: true }))
+
+    }
+
+    render() {
+        return (
+            this.state.isLoaded ?
+                <div className="research-page-company-overview-column-container">
+                    <ResearchHeader name={this.state.keyStats.companyName} tickerSymbol={this.state.tickerSymbol} price={this.state.price} marketcap={this.state.keyStats.marketcap} />
+                    <div className="company-overview">
+                        <CompanyOverview name={this.state.keyStats.companyName}/>
+                    </div>
+                    <div className="company-news">
+                        <CompanyNews name={this.state.keyStats.companyName}/>
+                    </div>
+                </div>
+                : <div></div>
+        );
+    }
 }
 
 
