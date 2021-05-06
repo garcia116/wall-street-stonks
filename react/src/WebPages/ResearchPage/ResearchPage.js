47118e654934c1bd8b18da808e7700ec6b8cc53b
USCS-529 <USCS-529@scs.com>
2021-05-05 22:48:26 -0700

    research page

--- react/src/WebPages/ResearchPage/ResearchPage.js
+++ react/src/WebPages/ResearchPage/ResearchPage.js
@@ -6,48 +6,73 @@ import Stats from '../../WebPages/ResearchPage/ResearchPageStats';
 import Dividends from '../../WebPages/ResearchPage/ResearchPageDividends';
 import PriceChart from '../../WebPages/ResearchPage/ResearchPagePriceChart';
 import CompanyOverview from '../../WebPages/ResearchPage/ResearchPageCompanyOverview';
+import Form from './Form.js'
+import iex from '../../config/iex.js'
 import './ResearchPage.css';
 
 class ResearchPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            tickerSymbol: 'TSLA',
+            tickerSymbol: '',
+            price: 0,
             keyStats: [{}],
             isLoaded: false
             
         }
     }
 
-    componentDidMount() {
-        var sandboxMode = true
-        var baseURL
-        var token
-        const keyStats = `/stock/${this.state.tickerSymbol}/stats/companyname?&token=`
-        
-        if (sandboxMode) {
-            baseURL = 'https://sandbox.iexapis.com/v1'
-            token = 'Tpk_a909e54fc2ab44ac976155957da2a605'
-        }
-        else {
-            baseURL = 'https://cloud.iexapis.com/v1'
-            token = 'pk_2d87808402a3463ab504dac6eb52b540'
-        }
+    componentDidUpdate(prevState) {
+        if (prevState.tickerSymbol !== this.state.tickerSymbol) {
 
-        const keyStatsURL = baseURL + keyStats + token
-       
-        fetch(keyStatsURL)
-            .then(response => response.json())
-            .then(data => this.setState({ keyStats: data, isLoaded: true }))
 
+            var sandboxMode = true
+            var baseURL
+            var token
+            const keyStats = `/stock/${this.state.tickerSymbol}/stats/?&token=`
+            const price = `/stock/${this.state.tickerSymbol}/price?&token=`
+
+
+            if (sandboxMode) {
+                baseURL = 'https://sandbox.iexapis.com/v1'
+                token = 'Tpk_a909e54fc2ab44ac976155957da2a605'
+            }
+            else {
+                baseURL = 'https://cloud.iexapis.com/v1'
+                token = 'pk_2d87808402a3463ab504dac6eb52b540'
+            }
+
+            const keyStatsURL = baseURL + keyStats + token
+            const priceURL = baseURL + price + token
+
+
+
+            fetch(keyStatsURL)
+                .then(response => response.json())
+                .then(data => this.setState({ keyStats: data, isLoaded: true }))
+            fetch(priceURL)
+                .then(response => response.json())
+                .then(data => this.setState({ price: data }))
         }
+    }
+
+    getStock = async (e) => {
+
+        const stock = e.target.stockName.value;
+        e.preventDefault();
+
+        this.setState({
+            tickerSymbol: stock
+        })
+        console.log(this.state.tickerSymbol)
+    }
        
     
     render() {
         return (
-            this.state.isLoaded?
                 <div className="research-container">
-                    <ResearchHeader name={this.state.keyStats.companyName} tickerSymbol={this.state.tickerSymbol} marketcap={this.state.keyStats.marketcap} />
+                <Form getStock={this.getStock} />
+                <ResearchHeader name={this.state.keyStats.companyName} price={this.state.price} tickerSymbol={this.state.tickerSymbol} marketcap={this.state.keyStats.marketcap} />
                     <BrowserRouter>
                         <div className="research-links">
                             <Link to="/ResearchPageFinancials">Financials</Link>
@@ -57,7 +82,6 @@ class ResearchPage extends Component {
                             <Link to="/ResearchPageCompanyOverview">Company Overview</Link>
                         </div>
                         <Switch>
-                            <Route path="/ResearchPage" component={Financials} />
                             <Route path="/ResearchPageFinancials" component={Financials} />
                             <Route path="/ResearchPageStats" component={Stats} />
                             <Route path="/ResearchPageDividends" component={Dividends} />
@@ -66,8 +90,7 @@ class ResearchPage extends Component {
                     </Switch>
                     </BrowserRouter>
                 </div>
-                :
-                <div>Loading...</div>
+               
             )
     }
 }
