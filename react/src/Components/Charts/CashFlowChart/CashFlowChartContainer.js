7be5eda1db902b31de2d81c7b88fc635388ff531
USCS-529 <USCS-529@scs.com>
2021-05-06 23:08:33 -0700

    cashflow container

--- react/src/Components/Charts/CashFlowChart/CashFlowChartContainer.js
+++ react/src/Components/Charts/CashFlowChart/CashFlowChartContainer.js
@@ -1,69 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CashFlowChartContainer.css';
 import CashFlowChart from "../../Charts/CashFlowChart/CashFlowChart.js";
+import axios from 'axios';
 
+function CashFlowChartContainer({ tickerSymbol }) {
 
-class CashFlowChartContainer extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            tickerSymbol: props.tickerSymbol,
-            cashFlow: [{}],
-            isLoaded: false
-        }
-    }
+    const [cashFlow, setCashFlow] = useState()
+    const [isLoaded, setLoaded] = useState()
 
-    componentDidMount() {
-        var sandboxMode = true
-        var baseURL
-        var token
+    useEffect(() => {
+        (async () => {
+            let mounted = true
+            if (mounted) {
+                var sandboxMode = true
+                var baseURL
+                var token
 
-        const cashFlow = `/stock/${this.state.tickerSymbol}/cash-flow?period=quarter&last=12&token=`
+                const cashFlow = `/stock/${tickerSymbol}/cash-flow?period=quarter&last=12&token=`
 
-        if (sandboxMode) {
-            baseURL = 'https://sandbox.iexapis.com/v1'
-            token = 'Tpk_a909e54fc2ab44ac976155957da2a605'
-        }
-        else {
-            baseURL = 'https://cloud.iexapis.com/v1'
-            token = 'pk_2d87808402a3463ab504dac6eb52b540'
-        }
+                if (sandboxMode) {
+                    baseURL = 'https://sandbox.iexapis.com/v1'
+                    token = 'Tpk_a909e54fc2ab44ac976155957da2a605'
+                }
+                else {
+                    baseURL = 'https://cloud.iexapis.com/v1'
+                    token = 'pk_2d87808402a3463ab504dac6eb52b540'
+                }
 
-        const cashFlowURL = baseURL + cashFlow + token
+                const cashFlowURL = baseURL + cashFlow + token
+                const result = await axios.get(cashFlowURL)
+                setCashFlow(result.data)
+                setLoaded(true)
 
-        fetch(cashFlowURL)
-            .then(response => response.json())
-            .then(data => this.setState({ cashFlow: data, isLoaded: true }))
-
-        function sleep(milliseconds) {
-            const date = Date.now();
-            let currentDate = null;
-            do {
-                currentDate = Date.now();
-            } while (currentDate - date < milliseconds);
-        }
-        sleep(100)
-    }
-
-    render() {
-        return (
-            this.state.isLoaded ?
-                <div className="cash-flow-chart-container">
-                    <p>Cash Flow</p>
-                    <div className="cash-flow-chart">
-                        <CashFlowChart cashFlow={this.state.cashFlow} />
-                    </div>
+                function sleep(milliseconds) {
+                    const date = Date.now();
+                    let currentDate = null;
+                    do {
+                        currentDate = Date.now();
+                    } while (currentDate - date < milliseconds);
+                }
+                sleep(100)
+            }
+            return () => mounted = false;
+        })();
+    }, []);
+    return (
+        isLoaded ?
+            <div className="cash-flow-chart-container">
+                <p>Cash Flow</p>
+                <div className="cash-flow-chart">
+                    <CashFlowChart cashFlow={cashFlow} />
                 </div>
-                :
-                <div className="cash-flow-chart-container">
-                    <p>Cash Flow</p>
-                    <div className="cash-flow-chart">
-                        <h1>Loading...</h1>
-                    </div>
+            </div>
+            :
+            <div className="cash-flow-chart-container">
+                <p>Cash Flow</p>
+                <div className="cash-flow-chart">
+                    <h1>Loading...</h1>
                 </div>
-        );
-    }
+            </div>
+    );
 }
-
-
 export default CashFlowChartContainer;
